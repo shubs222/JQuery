6317ce7285df9648df473a332a0a891fc6584cc9
shubham <shubhamtorvi18@gmail.com>
2018-09-19 19:55:46 +0530

    19thSep

---
+++ 18thSep2018/app.js
@@ -0,0 +1,39 @@
+﻿console.log('hello');
+
+$(document).ready(function () {
+    $("body").css("opacity", "0.5");
+    $(".centerDiv").css("background", "rgba(0,0,0,0.9)");
+    $("h1").css("text-align", "center");
+    $("#color").css("color", "white");
+    $("h1").click(function () {
+        $(this).hide();
+    });
+});
+
+$(document).ready(Sum);
+
+function Sum() {
+    $("#button1").click(function () {
+        if (($("#num1").val() >= 0) && ($("#num2").val() >= 0)) {
+            var a = parseInt($("#num1").val());
+            var b = parseInt($("#num2").val());
+            var sum = a + b;
+            alert(sum);
+            console.log(sum);
+        } 
+        else { alert("enter  values");}
+    });
+    siblings1();
+}
+function siblings1() {
+
+    console.log($(".CenterForm").siblings());
+}
+
+$(document).ready(function () {
+    $("button").click(function () {
+        $("#div1").fadeToggle();
+        $("#div2").fadeToggle("slow");
+    });
+});
+
---
+++ 19th_Sep2018/app.js
@@ -0,0 +1,62 @@
+﻿$(document).ready(function () {
+    $(".head1").click(function () {
+        //dbclick  
+        $("h1").addClass("head1new");
+    });
+
+    $(".head2").mouseenter(function () {
+
+        $("h2").addClass("head2new");
+    });
+
+    $(".head2").mouseleave(function () {
+
+        $("h2").css("color", "white");
+    });
+
+    $(".head3").on({
+
+        mouseenter: (function () {
+
+            $(".head3").css("color","yellow");
+        }),
+
+        mouseleave: (function () {
+
+            $(".head3").css("color","blue");
+        })
+    });
+    $("#button1").click(function () {
+        $("input[type=text]").val(null);
+        $("input[type=number]").val(null);
+        alert("clear");
+    });
+
+    //$("input").change(function () {
+    //        var $input = $(this);
+    //        $("p").html(".attr( 'checked' ): <b>" + $input.attr("checked") + "</b><br>" +
+    //            ".prop( 'checked' ): <b>" + $input.prop("checked") + "</b><br>" +
+    //            ".is( ':checked' ): <b>" + $input.is(":checked") + "</b>");
+    //});
+    $("#button2").click(function () {
+        $(".container").empty()
+    });
+    $(function () {
+        var rows = $("tr");
+        rows.filter(":even").css("background", "red","opacity","0.9");
+        rows.filter(":odd").css("background", "blue", "opacity", "0.9");
+    });
+    var filePath;
+    $('input[type=file]').change(function () {
+        filePath = $(this).val();
+        $("img").attr({
+            src: filePath,
+             title: "jQuery",
+            alt: "jQuery Logo",
+        });
+        console.log(filePath);
+        $("img").css("height", "200px", "width", "100px","display block");
+    });
+  
+})
+ 
